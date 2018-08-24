1c84bb3f7b6a183c198af9db5b219adc5a6d93bc
root <root@ip-172-31-91-102.ec2.internal>
2018-08-24 05:48:25 +0000

    Additional changes

--- login.java
+++ login.java
@@ -1,7 +1,7 @@
 class A {
  public static void main(String args[]){
-     System.out.println("Hello World");
-     System.out.println("Cape Demo");
-     System.out.println("Welcome!!");
+     System.out.println("Hello World")
+     System.out.println("Cape Demo"
+     System.out.println("Welcome!!")
  }
 }
